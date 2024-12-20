1ff51b4170f6dc076ee2445ae4552f8600fc0a26
Udit Mehta <uditmehta100@gmail.com>
2024-12-20 15:13:42 +0530

    api gateway added

---
+++ discovery-server/src/test/java/com/sdp/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdp.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
